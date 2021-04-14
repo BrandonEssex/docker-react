182bda6a6a2e6703342491c5f7d38e188a536681
Brandon Essex <bessex-c@3c.com>
2021-04-13 23:32:37 -0400

    App.js fix for test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Saved on Feature Branch
+          Learn React
         </a>
       </header>
     </div>
